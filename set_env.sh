9954f64f48cb38271dec2749f12f9adc76dc1147
tran-cam-long <camlong.tran0702@gmail.com>
2023-06-22 15:01:16 +0700

    First commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=12345678
+export POSTGRES_HOST=cloud-dev-prj3-psql.co6xjlda6j6k.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::cloud-dev-prj3-s3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
