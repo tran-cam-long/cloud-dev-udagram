358368e6e6a876864fc15328cc29a2bba3676fd2
tran-cam-long <camlong.tran0702@gmail.com>
2023-07-04 10:09:36 +0700

    final

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a3c7f433794474629b8de1527bbc8238-1687235495.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
